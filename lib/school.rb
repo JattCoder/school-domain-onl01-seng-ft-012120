6458cdfa73f9bcc9f8aaa93c07b39c2088d0aeca
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-01-31 15:33:22 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,7 @@
 class School
   def initialize(school_name)
     @school_name = school_name
-    @roster = Hash.new{ |h, k| h[k] = [] }
+    @roster = Hash.new
   end
   def add_student(student_name, grade)
   if @roster[grade]
