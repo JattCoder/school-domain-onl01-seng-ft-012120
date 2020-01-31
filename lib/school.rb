e908b0c8136eb5394fe31f6e643edee1788844f9
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-01-31 14:50:13 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,13 @@
-# code here!
+class School
+  attr_reader :student
+  attr_writer :student
+  def initialize(roaster)
+
+  end
+  def roaster
+    @student.length
+  end
+end
+
+school = School.new("Test School")
+school.roaster
